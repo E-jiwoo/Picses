8f23a357f2b0fba00fc58ab683224877466bc67b
E-jiwoo <livei07@naver.com>
2023-11-27 20:13:44 +0900

    ADD :: 예 아니오 버튼 추가 - 확인 페이지에 예 아니오 버튼을 추가해주었다

--- book/src/Pages/Check/RegisterCheck/index.js
+++ book/src/Pages/Check/RegisterCheck/index.js
@@ -8,10 +8,62 @@ const RegisterCheck = () => {
         ‘콩쥐팥쥐’
         <p />: 다음의 도서가 등록을 원하는 책이 맞나요?
       </Font>
+      <OKButton>
+        <WhiteFont>예</WhiteFont>
+      </OKButton>
+      <NoButton>
+        <BlackFont>아니오</BlackFont>
+      </NoButton>
     </div>
   );
 };
 
+const WhiteFont = styled.div`
+  color: var(--gray-scale-gㅣray-scale-700, #fff);
+  text-align: center;
+  font-family: SUITE;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 600;
+  padding: 3px;
+`;
+const BlackFont = styled.div`
+  color: var(--gray-scale-gray-scale-200, #262636);
+  text-align: center;
+  font-family: SUITE;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: 600;
+  padding: 3px;
+`;
+const OKButton = styled.button`
+  display: flex;
+  position: relative;
+  left: 190px;
+  top: 50px;
+  width: 75px;
+  padding: 8px 0px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 8px;
+  background: #5cb0ff;
+  border: none;
+`;
+const NoButton = styled.button`
+  display: flex;
+  position: relative;
+  left: 300px;
+  top: 5px;
+  width: 75px;
+  padding: 8px 0px;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  border-radius: 8px;
+  background: #fff;
+  border: 2px solid #5cb0ff;
+`;
 const Font = styled.div`
   color: #000;
   font-family: SUIT;
